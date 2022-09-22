68ade38513ad6349fab9b3cb34da5df4bfb8c9b9
mariamiakobidze <mariamiakobidze4@gmail.com>
2022-09-22 20:32:48 +0400

    fifth commit

---
+++ day 27/lesson1.py
@@ -0,0 +1,48 @@
+# my_arr =[]
+
+# my_arr.append("nika")
+# my_arr.append("luka")
+# my_arr.append("dito")
+
+# #for element in my_arr:
+# #    print(element)
+
+# for i in range (len(my_arr)):
+#     print (my_arr[i])
+    
+# print([i for i in my_arr])
+
+
+# my_arr= [i for i in range(10, 20)if i % 2 == 1]
+
+# print (my_arr)
+
+#list
+
+
+#dictionary სიის ერთ ერთი სახეობა
+
+# dict_of_students = {
+#     "nika keshelava" : "male" ,
+#     "mariam qsovreli" : "female",
+#     "children": ["andria", "zuka", "nikusha", "mariami"],
+#     }
+
+#key: value pair 
+#გასაღები: მნიშვნელოვანი წყვილი 
+
+#გასაღები ყოველთვის უნდა იყოს str ტიპის 
+#მნიშვნელობა შეიძლება იყოს ნებისმიერი ტიპის (დაჟე სიის)
+#ყოველი წყვილის შემდეგ იწერება მძიმე 
+# print(dict_of_students["children"][0])
+
+
+
+
+
+
+
+
+
+
+
---
+++ day 27/lesson2.py
@@ -0,0 +1,5 @@
+def never(arr):
+    i=0 
+    while i < len(arr)-10:
+        if arr[i+10] - arr[i] != 10
+           return arr 
